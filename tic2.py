a7fd6adaea8ff35e2b0879d69f0303c64500dc7b
Amy Roberts <uncamy@gmail.com>
2013-10-01 15:07:42 -0400

    new board option

---
+++ tic2.py
@@ -0,0 +1,15 @@
+
+
+
+board = [' ']* 10
+
+def print_board(board):
+    #board is a list of 10 strings ignoring index 0
+    print (' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print ('-----------')
+    print (' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print ('-----------')
+    print (' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+
+
+print_board(board)
